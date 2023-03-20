0154097c463c0b22e01df9fabbbd0b415f2dc25c
vipersak <124049980+vipersak@users.noreply.github.com>
2023-03-20 02:49:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ import nodemailer from'nodemailer';
 const port = 5000;
 
 import fetch from 'node-fetch';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
 
